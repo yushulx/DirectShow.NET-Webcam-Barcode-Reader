300de13ca2f8325350aebe33bd0b0eeb2672a474
yushulx <lingxiao1002@gmail.com>
2016-09-27 16:11:00 +0800

    first commit

---
+++ BarcodeReader/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BarcodeReaderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.AutoEllipsis = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(660, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AllowDrop = true;
+            this.button2.AutoEllipsis = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(660, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Read Barcode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(660, 220);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(250, 430);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.AllowDrop = true;
+            this.button3.AutoEllipsis = true;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(660, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 64);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start Webcam";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(919, 661);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Barcode Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# DirectShow.NET Webcam Barcode Reader
+
+The sample demonstrates how to implement a simple C# barcode reader on Windows using [DirectShow.NET][1] and [Dynamsoft Barcode Reader SDK][2] (DBR).
+
+![DirectShow.NET Webcam barcode reader](http://www.codepool.biz/wp-content/uploads/2016/09/directshow.PNG)
+
+## Getting Started
+1. Download [DirectShow.NET][3].
+2. Import the Project to **Visual Studio 2015**.
+3. Add **DBR** to project via **Nuget**.
+
+    ![install DBR via nuget](http://www.codepool.biz/wp-content/uploads/2016/09/nuget-install-dbr.PNG)
+4. Add **DirectShowLib-2005.dll** to reference.
+5. Connect a USB camera to your PC.
+6. Build and run the project.
+
+[1]:http://directshownet.sourceforge.net/index.html
+[2]:http://www.dynamsoft.com/Products/Dynamic-Barcode-Reader.aspx
+[3]:https://sourceforge.net/projects/directshownet/files/
